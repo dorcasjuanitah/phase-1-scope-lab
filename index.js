84eb236b972b90be33f548567eb5d588e6796b95
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-21 12:42:38 +0300

    variable scopes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var customerName = 'bob';
+var bestCustomer ='not bob';
+const LeastFavoriteCustomer = '';
+
+function upperCaseCustomerName(){
+     return customerName = customerName.toUpperCase();
+} 
+
+function setBestCustomer() {
+    var bestCustomer ='not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    var bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+return LeastFavoriteCustomer = LeastFavoriteCustomer.toUpperCase();
+}
 // Write your solution in this file!
